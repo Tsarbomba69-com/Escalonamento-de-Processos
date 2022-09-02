1fa5bef16e0622cb5cad06f0a229292f418aecd6
hendrickdias66 <hendrickdias66@gmail.com>
2022-09-02 17:09:04 +0100

    Loading screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private TextMeshProUGUI progressText;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private Image imageComp;
+    [SerializeField] private Text text;
+    [SerializeField] private Text textNormal;
+
+
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        mainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    private IEnumerator LoadLevel(int levelIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            text.text = Math.Truncate(progress * 100) + "%";
+            imageComp.fillAmount = progress;
+            textNormal.text = "Carregando...";
+            yield return null;
+        }
     }
 
     public void Quit()
